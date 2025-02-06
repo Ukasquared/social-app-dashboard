2a381cf6087af7062a65b565676bcbe4883ff3d3
judithukachukwu <ukachukwujudith95@gmail.com>
2025-02-06 15:16:22 +0100

    navigate path / to /home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./Layouts/Home"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import "./assets/styles.css"
 import HomeOutlet from "./view/home"
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />}/>
         <Route path="/home" element={<Home />}> 
           <Route index element={<HomeOutlet />} />
         </Route>
